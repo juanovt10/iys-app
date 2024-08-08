5510d919e738db0f091d6a32e72b40d3e5927904
Juan Velasquez <juanovelasquez10@gmail.com>
2024-08-08 12:55:34 +0200

    Finalize item form implementation with toast error messages and quantity and price validation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/themes.provider";
+import { Toaster } from "@/components/ui/toaster"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -30,6 +31,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           {children}
 
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
--- components/ItemsForm.tsx
+++ components/ItemsForm.tsx
@@ -1,306 +1,4 @@
-// // ItemsForm.tsx
-
-// import React, { useState, useEffect } from 'react';
-// import { createClient } from '@supabase/supabase-js';
-// import { useForm, Controller } from 'react-hook-form';
-// import { Button } from '@/components/ui/button';
-// import { Input } from '@/components/ui/input';
-// import supabase from '@/lib/supabase/client';
-// import { Item } from '@/types';
-
-
-
-
-
-// const ItemsForm = ({ nextStep, prevStep, updateFormData, itemsData }: any) => {
-//   const [filteredItems, setFilteredItems] = useState<Item[]>([]);
-//   const [selectedItems, setSelectedItems] = useState<Item[]>(itemsData || []);
-
-//   const { control, handleSubmit, reset } = useForm({
-//     defaultValues: {
-//       search: '',
-//       quantity: 1,
-//       price: 0,
-//     },
-//   });
-
-//   useEffect(() => {
-//     if (itemsData) {
-//       setSelectedItems(itemsData);
-//     }
-//   }, [itemsData]);
-
-//   const fetchItems = async (searchTerm: string) => {
-//     const { data, error } = await supabase
-//       .from('items')
-//       .select('*')
-//       .or(`descripcion.ilike.%${searchTerm}%,codigo.ilike.%${searchTerm}%,color.ilike.%${searchTerm}%,tamano.ilike.%${searchTerm}%,categoria.ilike.%${searchTerm}%`)
-
-//     if (data) {
-//       setFilteredItems(data);
-//     } else {
-//       console.error('Error fetching items:', error);
-//     }
-//   };
-
-//   const onSearch = (value: string) => {
-//     if (value.length >= 3) { // Fetch only if the search term has 3 or more characters
-//       fetchItems(value);
-//     } else {
-//       setFilteredItems([]);
-//     }
-//   };
-
-//   const onSelectItem = (item: Item) => {
-//     reset({ search: ''});
-//     setFilteredItems([]);
-//     setSelectedItems([...selectedItems, { ...item, cantidad: item.cantidad }]);
-//   };
-
-//   const onUpdateItem = (index: number, field: keyof Item, value: any) => {
-//     const updatedItems = selectedItems.map((item, idx) =>
-//       idx === index ? { ...item, [field]: value } : item
-//     );
-//     setSelectedItems(updatedItems);
-//   };
-
-//   const onSubmit = () => {
-//     updateFormData({ items: selectedItems });
-//     nextStep();
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
-//         <div>
-
-//           {/* search component */}
-//           <Controller
-//             name="search"
-//             control={control}
-//             render={({ field }) => (
-//               <Input
-//                 {...field}
-//                 placeholder="Search items"
-//                 onChange={(e) => {
-//                   field.onChange(e);
-//                   onSearch(e.target.value);
-//                 }}
-//               />
-//             )}
-//           />
-
-//           {filteredItems.length > 0 && (
-
-//             // dropdown component
-//             <ul className="dropdown">
-//               {filteredItems.map(item => (
-//                 <li key={item.id} onClick={() => onSelectItem(item)}>
-//                   {item.descripcion}
-//                 </li>
-//               ))}
-//             </ul>
-//           )}
-//         </div>
-
-//         {selectedItems.map((item, index) => (
-//           // selected item component
-
-//           <div key={index} className="item-input">
-//             <div>{item.descripcion}</div>
-//             <Input
-//               type="number"
-//               placeholder="Quantity"
-//               value={item.cantidad}
-//               onChange={(e) => onUpdateItem(index, 'cantidad', e.target.value)}
-//             />
-//             <Input
-//               type="number"
-//               placeholder="Price"
-//               value={item.precio_unidad}
-//               onChange={(e) => onUpdateItem(index, 'precio_unidad', e.target.value)}
-//             />
-//           </div>
-//         ))}
-
-//         <div className="flex justify-between">
-//           <Button type="button" onClick={prevStep}>Back</Button>
-//           <Button type="submit">Continar</Button>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default ItemsForm;
-
-
-// import React, { useState, useEffect } from 'react';
-// import { useForm, Controller } from 'react-hook-form';
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { z } from 'zod';
-// import { Check, ChevronsUpDown } from "lucide-react";
-
-// import { Button } from '@/components/ui/button';
-// import { Input } from '@/components/ui/input';
-// import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
-// import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
-// import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-// import supabase from '@/lib/supabase/client';
-// import { Item } from '@/types';
-// import { cn } from '@/lib/utils';
-
-// const FormSchema = z.object({
-//   // search: z.string().min(3, "Please enter at least 3 characters to search."),
-//   quantity: z.number().min(1, "Quantity must be at least 1."),
-//   price: z.number().min(0, "Price cannot be negative."),
-// });
-
-// const ItemsForm = ({ nextStep, prevStep, updateFormData, itemsData }: any) => {
-//   const [filteredItems, setFilteredItems] = useState<Item[]>([]);
-//   const [selectedItems, setSelectedItems] = useState<Item[]>(itemsData || []);
-
-//   const form = useForm({
-//     resolver: zodResolver(FormSchema),
-//     defaultValues: {
-//       search: '',
-//       quantity: 1,
-//       price: 0,
-//     },
-//   });
-
-//   useEffect(() => {
-//     if (itemsData) {
-//       setSelectedItems(itemsData);
-//     }
-//   }, [itemsData]);
-
-//   useEffect(() => {
-//     const fetchAllItems = async () => {
-//       const { data, error } = await supabase
-//         .from('items')
-//         .select('*');
-
-//       if (data) {
-//         setFilteredItems(data);
-//       } else {
-//         console.error('Error fetching items:', error);
-//       }
-//     };
-
-//     fetchAllItems();
-//   }, []);
-
-//   const onSelectItem = (item: Item) => {
-//     form.reset({ search: '' });
-//     setSelectedItems([...selectedItems, { ...item, cantidad: item.cantidad || 1, precio_unidad: item.precio_unidad || 0 }]);
-//   };
-
-//   const onUpdateItem = (index: number, field: keyof Item, value: any) => {
-//     const updatedItems = selectedItems.map((item, idx) =>
-//       idx === index ? { ...item, [field]: field === 'cantidad' || field === 'precio_unidad' ? Number(value) : value } : item
-//     );
-//     setSelectedItems(updatedItems);
-//   };
-
-// const onSubmit = (data: any) => {
-//   updateFormData({ items: selectedItems });
-//   nextStep();
-// };
-  
-//   return (
-//     <div>
-//       <Form {...form}>
-//         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-//           <FormField
-//             control={form.control}
-//             name="search"
-//             render={({ field }) => (
-//               <FormItem className="flex flex-col">
-//                 <FormLabel>Buscar Items</FormLabel>
-//                 <Popover>
-//                   <PopoverTrigger asChild>
-//                     <FormControl>
-//                       <Button
-//                         variant="outline"
-//                         role="combobox"
-//                         className={cn(
-//                           "w-full justify-between border-red-500 bg-red-50 cursor-pointer",
-//                           !field.value && "text-muted-foreground"
-//                         )}
-//                       >
-//                         {field.value
-//                           ? filteredItems.find((item) => item.descripcion === field.value)?.descripcion
-//                           : "Agregar item"}
-//                         <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-//                       </Button>
-//                     </FormControl>
-//                   </PopoverTrigger>
-//                   <PopoverContent className="w-full p-0">
-//                     <Command>
-//                       <CommandInput placeholder="Buscar items..." />
-//                       <CommandList>
-//                         <CommandEmpty>No se encontraron items.</CommandEmpty>
-//                         <CommandGroup>
-//                           {filteredItems
-//                             .filter(item => item.descripcion.toLowerCase().includes(field.value.toLowerCase()))
-//                             .map((item) => (
-//                               <CommandItem
-//                                 value={item.descripcion}
-//                                 key={item.id}
-//                                 onSelect={() => onSelectItem(item)}
-//                               >
-//                                 <Check className={`mr-2 h-4 w-4 ${String(item.id) === field.value ? "opacity-100" : "opacity-0"}`} />
-//                                 {item.descripcion}
-//                               </CommandItem>
-//                             ))}
-//                         </CommandGroup>
-//                       </CommandList>
-//                     </Command>
-//                   </PopoverContent>
-//                 </Popover>
-//                 <FormMessage />
-//               </FormItem>
-//             )}
-//           />
-
-//           <div className="space-y-4">
-//             {selectedItems.map((item, index) => (
-//               <div key={index} className="flex items-center space-x-4 border p-4 rounded-lg">
-//                 <div className="flex-1">{item.descripcion}</div>
-//                 <Input
-//                   type="number"
-//                   placeholder="Quantity"
-//                   value={item.cantidad}
-//                   onChange={(e) => onUpdateItem(index, 'cantidad', Number(e.target.value))}
-//                   className="w-24"
-//                 />
-//                 <Input
-//                   type="number"
-//                   placeholder="Price"
-//                   value={item.precio_unidad}
-//                   onChange={(e) => onUpdateItem(index, 'precio_unidad', Number(e.target.value))}
-//                   className="w-24"
-//                 />
-//               </div>
-//             ))}
-//           </div>
-
-//           <div className="flex justify-between">
-//             <Button type="button" onClick={prevStep}>Back</Button>
-//             <Button type="submit">Continue</Button>
-//           </div>
-//         </form>
-//       </Form>
-//     </div>
-//   );
-// };
-
-// export default ItemsForm;
-
-
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from 'zod';
@@ -314,19 +12,27 @@ import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover
 import supabase from '@/lib/supabase/client';
 import { Item } from '@/types';
 import { cn } from '@/lib/utils';
+import { useToast } from '@/components/ui/use-toast';
+import { ToastAction } from './ui/toast';
+import { ServerInsertedHTMLContext } from 'next/navigation';
 
 const FormSchema = z.object({
-  // No need to validate search in the schema
+  items: z.array(z.object({
+    cantidad: z.number().min(1, "Quantity must be at least 1."),
+    precio_unidad: z.number().min(1, "Price cannot be negative.")
+  })).nonempty("You must add at least one item."),
 });
 
 const ItemsForm = ({ nextStep, prevStep, updateFormData, itemsData }: any) => {
   const [filteredItems, setFilteredItems] = useState<Item[]>([]);
   const [selectedItems, setSelectedItems] = useState<Item[]>(itemsData || []);
   const [searchTerm, setSearchTerm] = useState('');
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const { toast } = useToast();
 
   const form = useForm({
     resolver: zodResolver(FormSchema),
-    defaultValues: {},
+    defaultValues: { items: itemsData || [] },
   });
 
   useEffect(() => {
@@ -351,34 +57,86 @@ const ItemsForm = ({ nextStep, prevStep, updateFormData, itemsData }: any) => {
     fetchAllItems();
   }, []);
 
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
-  };
-
   const onSelectItem = (item: Item) => {
+    const isDuplicate = selectedItems.some(selectedItem => selectedItem.id === item.id);
+
+    if (isDuplicate) {
+      toast({
+        variant: "destructive",
+        title: "Item duplicado",
+        description: "Este item ya ha sido agregado.",
+        action: <ToastAction altText="Dismiss">Cerrar</ToastAction>,
+      });
+      return;
+    }
+
     setSearchTerm('');
-    setSelectedItems([...selectedItems, { ...item, cantidad: item.cantidad || 1, precio_unidad: item.precio_unidad || 0 }]);
+    setIsPopoverOpen(false);
+    const newItems = [{ ...item, cantidad: item.cantidad || 1, precio_unidad: item.precio_unidad || 0 }, ...selectedItems];
+    setSelectedItems(newItems);
+    form.setValue('items', newItems);
   };
 
   const onUpdateItem = (index: number, field: keyof Item, value: any) => {
     const updatedItems = selectedItems.map((item, idx) =>
-      idx === index ? { ...item, [field]: field === 'cantidad' || field === 'precio_unidad' ? Number(value) : value } : item
+      idx === index ? { ...item, [field]: Number(value) } : item
     );
     setSelectedItems(updatedItems);
+    form.setValue('items', updatedItems);
   };
 
-  const onSubmit = () => {
+  const onRemoveItem = (index: number) => {
+    const updatedItems = selectedItems.filter((_, idx) => idx !== index);
+    setSelectedItems(updatedItems);
+    form.setValue('items', updatedItems);
+  };
+
+  const handleSubmitClick = async () => {
+    const isValid = await form.trigger();
+    if (!isValid) {
+      selectedItems.forEach((item, index) => {
+        const itemErrors = (form.formState.errors.items as any)?.[index] ?? {};
+        const cantidadError = itemErrors.cantidad?.message;
+        const precioError = itemErrors.precio_unidad?.message;
+        const itemNumber = selectedItems.length - index;
+
+        if (cantidadError) {
+          toast({
+            variant: "destructive",
+            title: `Item ${itemNumber} - Quantity Error`,
+            description: String(cantidadError),
+            action: <ToastAction altText="Try again">Try again</ToastAction>,
+          });
+        }
+
+        if (precioError) {
+          toast({
+            variant: "destructive",
+            title: `Item ${itemNumber} - Price Error`,
+            description: String(precioError),
+            action: <ToastAction altText="Try again">Try again</ToastAction>,
+          });
+        }
+      });
+      return;
+    }
+
+    // If no validation errors, submit the form
+    form.handleSubmit(onSubmit)();
+  };
+
+  const onSubmit = (data: any) => {
     updateFormData({ items: selectedItems });
     nextStep();
   };
 
   return (
-    <div>
+    <div className="relative">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormItem className="flex flex-col">
             <FormLabel>Buscar Items</FormLabel>
-            <Popover>
+            <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
               <PopoverTrigger asChild>
                 <FormControl>
                   <Button
@@ -424,31 +182,53 @@ const ItemsForm = ({ nextStep, prevStep, updateFormData, itemsData }: any) => {
             <FormMessage />
           </FormItem>
 
-          <div className="space-y-4">
+          <div className="space-y-4 max-h-80 overflow-y-auto">
             {selectedItems.map((item, index) => (
-              <div key={index} className="flex items-center space-x-4 border p-4 rounded-lg">
-                <div className="flex-1">{item.descripcion}</div>
-                <Input
-                  type="number"
-                  placeholder="Quantity"
-                  value={item.cantidad}
-                  onChange={(e) => onUpdateItem(index, 'cantidad', Number(e.target.value))}
-                  className="w-24"
-                />
-                <Input
-                  type="number"
-                  placeholder="Price"
-                  value={item.precio_unidad}
-                  onChange={(e) => onUpdateItem(index, 'precio_unidad', Number(e.target.value))}
-                  className="w-24"
-                />
+              <div
+              key={index}
+              className="flex flex-col space-y-4 border p-6 rounded-md bg-white shadow-md"
+            >
+              <div className="flex justify-between items-start">
+                <div className="flex-1 font-medium text-gray-800">
+                  #{selectedItems.length - index} - {item.descripcion}
+                </div>
+                <div className="flex items-start space-x-4">
+                  <div className="flex flex-col items-end space-y-2">
+                    <label htmlFor={`quantity-${index}`} className="text-gray-600 text-sm">
+                      Cantidad ({item.unidad})
+                    </label>
+                    <input
+                      id={`quantity-${index}`}
+                      type="number"
+                      placeholder="Quantity"
+                      value={item.cantidad}
+                      onChange={(e) => onUpdateItem(index, 'cantidad', e.target.value)}
+                      className="px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-24"
+                    />
+                  </div>
+                  <div className="flex flex-col items-end space-y-2">
+                    <label htmlFor={`price-${index}`} className="text-gray-600 text-sm">
+                      Precio/{item.unidad}
+                    </label>
+                    <input
+                      id={`price-${index}`}
+                      type="number"
+                      placeholder="Price"
+                      value={item.precio_unidad}
+                      onChange={(e) => onUpdateItem(index, 'precio_unidad', e.target.value)}
+                      className="px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-24"
+                    />
+                  </div>
+                </div>
+              </div>
+                <Button variant="destructive" type='button' onClick={() => onRemoveItem(index)}>Remove</Button>
               </div>
             ))}
           </div>
 
-          <div className="flex justify-between">
+          <div className="flex justify-between pt-4">
             <Button type="button" onClick={prevStep}>Back</Button>
-            <Button type="submit">Continue</Button>
+            <Button type="button" onClick={handleSubmitClick}>Continue</Button>
           </div>
         </form>
       </Form>
